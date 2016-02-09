148f23793d9501196ec4919b7edd845c40f4d614
carolineevail <carolineevail@gmail.com>
2016-02-09 11:55:03 -0500

    Add battle system

---
+++ src/Enemy.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Caroline on 2/9/16.
+ */
+public class Enemy extends Character {
+    public Enemy(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -3,16 +3,20 @@ import java.util.ArrayList;
 /**
  * Created by Caroline on 2/3/16.
  */
-public class Player {
-    String name;
+public class Player extends Character {
     String weapon;
     String location;
     ArrayList<String> items = new ArrayList<>();
 
+    public Player() {
+        this.health = 20;
+        this.damage = 20;
+    }
+
     public void chooseName() {
         System.out.println("What is your name?");
         name = Game.nextLine();
-        System.out.println("Welcome, " + name);
+        System.out.printf("Welcome, %s\n", name);
     }
 
     public void chooseWeapon() throws Exception {
@@ -46,7 +50,7 @@ public class Player {
     }
 
     public void findItem(String item) {
-        System.out.println("You found a " + item + "! Pick it up? [y/n]");
+        System.out.printf("You found a %s! Pick it up? [y/n]\n", item);
         String answer = Game.nextLine();
         if (answer.equalsIgnoreCase("y")) {
             items.add(item);
